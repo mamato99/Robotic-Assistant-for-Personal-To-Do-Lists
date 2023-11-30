33922c6d68db83e32aff5074e1d9c7d3e56ec259
mamato99 <152490388+mamato99@users.noreply.github.com>
2023-11-30 15:21:32 +0100

    Delete tablet_pkg directory

--- tablet_pkg/scripts/rosbridge_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-WS_DIR=$( cd -- "$(dirname "${0}")" >/dev/null 2>&1 ; pwd -P )
-
-cd "$WS_DIR/../../.."
-
-source /opt/ros/noetic/setup.bash
-roslaunch rosbridge_server rosbridge_websocket.launch
