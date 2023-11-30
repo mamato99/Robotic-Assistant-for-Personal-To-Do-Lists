439cc7206fef139fa9f0f0e4e3be43efd91efbc6
mamato99 <152490388+mamato99@users.noreply.github.com>
2023-11-30 15:21:05 +0100

    Delete rasa_ros directory

--- rasa_ros/src/rasa_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BOT_DIR=$( cd -- "$(dirname "${0}")" >/dev/null 2>&1 ; pwd -P )
-
-cd "$BOT_DIR/../chatbot"
-
-rasa run -m models --endpoints endpoints.yml --port 5002 --credentials credentials.yml --enable-api
--- rasa_ros/src/rasa_server_ducklink.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BOT_DIR=$( cd -- "$(dirname "${0}")" >/dev/null 2>&1 ; pwd -P )
-
-cd "$BOT_DIR/../chatbot/duckling"
-
-stack exec duckling-example-exe
